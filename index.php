b09eeea6596c8bbed033e27b7776d837d0818448
joregesosa <joregesosa@gmail.com>
2025-02-03 18:32:33 -0400

    Load environment variables using Dotenv in index.php

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
+
+use Dotenv\Dotenv;
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL); 
+error_reporting(E_ALL);
 
 require 'vendor/autoload.php';
-require 'src/routes/index.php'; 
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+require 'src/routes/index.php';
