887786f9cd223b2f22081ff486250d1b5a81c44c
joregesosa <joregesosa@gmail.com>
2025-02-03 10:27:46 -0400

    Add database seed factories and initialization script

---
+++ src/database/seed/factories.php
@@ -0,0 +1,11 @@
+<?php
+
+$factories = [
+    "roles" => "App\Modules\Role\Factory",
+    "schools" => "App\Modules\School\Factory",
+    "countries" => "App\Modules\Country\Factory",
+    "categories" => "App\Modules\Category\Factory",
+    "users" => "App\Modules\User\Factory",
+    "services" => "App\Modules\Service\Factory",
+    "school_user" => "App\Modules\SchoolUser\Factory",
+];
