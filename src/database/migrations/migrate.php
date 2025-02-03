c230e635c6b6442ac9bf22b46f84a47bf89a9547
joregesosa <joregesosa@gmail.com>
2025-02-03 10:27:33 -0400

    Add migration management scripts for database operations

---
+++ src/database/migrations/migrate.php
@@ -0,0 +1,79 @@
+<?php
+
+use App\Config\DB;
+
+require __DIR__ . "/migrations.php";
+require "vendor/autoload.php";
+
+DB::initialize();
+
+$action = $argv[1] ?? "up";
+$migration = $argv[2] ?? null;
+
+if ($migration) {
+    $migration = ucfirst($migration);
+    if (isset($migrations[$migration])) {
+        executeMigration($migrations[$migration], $action);
+    } else {
+        echo "Migration $migration not found. \n";
+    }
+} else {
+    runMigrations($action);
+}
+
+function runMigrations($action)
+{
+    global $migrations;
+
+    foreach ($migrations as $migration => $class) {
+        executeMigration($class, $action);
+    }
+}
+
+function executeMigration($migration, $action)
+{
+    switch ($action) {
+        case "up":
+            up($migration);
+            break;
+        case "down":
+            down($migration);
+            break;
+        case "refresh":
+            refresh($migration);
+            break;
+        default:
+            echo "Invalid action";
+    }
+}
+
+function up($migration)
+{
+    try {
+        $migration::createTable();
+        echo "Table created successfully \n";
+    } catch (\Exception $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+
+function down($migration)
+{
+    try {
+        $migration::dropTable();
+        echo "Table dropped successfully \n";
+    } catch (\Exception $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+
+function refresh($migration)
+{
+    try {
+        $migration::dropTable();
+        $migration::createTable();
+        echo "Table refreshed successfully \n";
+    } catch (\Exception $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
---
+++ src/database/migrations/migrations.php
@@ -0,0 +1,10 @@
+<?php
+$migrations = [
+    "Roles" => "App\Modules\Role\Migration",
+    "Schools" => "App\Modules\School\Migration",
+    "Countries" => "App\Modules\Country\Migration",
+    "Categories" => "App\Modules\Category\Migration",
+    "Users" => "App\Modules\User\Migration",
+    "Services" => "App\Modules\Service\Migration",
+    "SchoolUsers" => "App\Modules\SchoolUser\Migration",
+];
