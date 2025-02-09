461ddf132cdc4a2431360218e72a53f8c6150f6d
joregesosa <joregesosa@gmail.com>
2025-02-09 17:15:14 -0400

    Add Student controller with index and show methods for student retrieval and role validation

---
+++ src/Modules/Student/Controller.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Modules\Student;
+
+use App\Modules\User\Model;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Middlewares\RoleAccess;
+
+class Controller
+{
+    public function index()
+    {
+        try {
+            RoleAccess::notStudent();
+            $query = Model::query();
+            $query->where('role_id', 4);
+            // ->where('status', 1);
+
+            if (isset($_GET['s'])) {
+                $query->whereHas('schools', function ($q) {
+                    $q->where('id', $_GET['s']);
+                });
+            }
+
+            $users = $query->get()->load('role');
+
+            header("HTTP/1.0 200 OK");
+            echo json_encode($users);
+        } catch (\Throwable $th) {
+            header("HTTP/1.0 500 Internal Server Error");
+            echo json_encode(['status' => 'error', 'message' => $th->getMessage()]);
+        }
+    }
+
+    public function show($id)
+    {
+        try {
+            RoleAccess::adminOrOwner($id);
+            $user = Model::findOrFail($id)->load(['role', 'schools', 'services']);
+            if($user->role->id != 4){
+                throw new ModelNotFoundException('Student not Found');
+            }
+            header("HTTP/1.0 200 OK");
+            echo json_encode($user);
+        } catch (ModelNotFoundException $th) {
+            header("HTTP/1.0 404 Not Found");
+            echo json_encode('Student not Found');
+        }catch (\Throwable $th) {
+            header("HTTP/1.0 500 Internal Server Error");
+            echo json_encode(['status' => 'error', 'message' => $th->getMessage()]);
+        }
+
+    }
+
+ 
+}
